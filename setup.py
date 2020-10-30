9bbf42a73005cd163a71fabc36657215dbe67428
mauricioAyllon <58120060+mauricioAyllon@users.noreply.github.com>
2020-10-30 12:19:07 -0400

    setup file for pip installable module

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+# import os
+
+setup(
+    name="nasagamma",
+    use_scm_version={"write_to": "nasagamma/version.py"},
+    description="Gamma spectroscopy tools",
+    url="https://github.com/mauricioAyllon/NASA-gamma",
+    author="Mauricio Ayllon Unzueta",
+    author_email="mauri.ayllon12@gmail.com",
+    packages=["nasagamma"],
+)
