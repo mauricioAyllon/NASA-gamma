a2e60aaf6bdc13835869c65a1a2843f8dc4cc249
mauricioAyllon <58120060+mauricioAyllon@users.noreply.github.com>
2021-03-09 09:34:37 -0500

    helper for gammaGUI-qt

---
+++ mplwidget.py
@@ -0,0 +1,22 @@
+# ------------------------------------------------------
+# -------------------- mplwidget.py --------------------
+# ------------------------------------------------------
+from PyQt5.QtWidgets import *
+
+from matplotlib.backends.backend_qt5agg import FigureCanvas
+
+from matplotlib.figure import Figure
+
+
+class MplWidget(QWidget):
+    def __init__(self, parent=None):
+
+        QWidget.__init__(self, parent)
+
+        self.canvas = FigureCanvas(Figure())
+
+        vertical_layout = QVBoxLayout()
+        vertical_layout.addWidget(self.canvas)
+
+        # self.canvas.axes = self.canvas.figure.add_subplot(111)
+        self.setLayout(vertical_layout)
