ecefa36672dd732f6cf7eb56b1f4ac3371824c45
mauricioAyllon <58120060+mauricioAyllon@users.noreply.github.com>
2021-06-08 09:34:23 -0400

    added energy slicing and fixed some things

--- nasagamma/tlist.py
+++ nasagamma/tlist.py
@@ -11,13 +11,17 @@ import matplotlib.pyplot as plt
 
 
 class Tlist:
-    def __init__(self, fname, period, ebins=4096):
+    def __init__(self, fname, period):
+        ebin_lst = [2 ** 10, 2 ** 11, 2 ** 12, 2 ** 13, 2 ** 14, 2 ** 15]
         self.period = period
-        self.ebins = ebins
         self.tbins = 200
         self.trange = [0, period]
+        self.erange = None
         self.data = self.load_data(fname)
+        self.ebins = min(ebin_lst, key=lambda x: abs(x - self.data[:, 0].max()))
+        self.dt_bins = 100
         self.df = pd.DataFrame(data=self.data, columns=["channel", "ts", "dt"])
+        self.spect_cts = 0
         plt.rc("font", size=14)
         plt.style.use("seaborn-darkgrid")
 
@@ -29,8 +33,16 @@ class Tlist:
         return data
 
     def filter_data(self, trange):
+        self.restore_df()
         self.df = self.df[(self.df["dt"] > trange[0]) & (self.df["dt"] < trange[1])]
         self.trange = trange
+
+    def filter_edata(self, erange):
+        self.restore_df()
+        self.df = self.df[
+            (self.df["channel"] > erange[0]) & (self.df["channel"] < erange[1])
+        ]
+        self.erange = erange
 
     def restore_df(self):
         self.df = pd.DataFrame(data=self.data, columns=["channel", "ts", "dt"])
@@ -50,20 +62,41 @@ class Tlist:
 
     def plot_vlines(self, ax=None):
         ax.axvspan(xmin=self.trange[0], xmax=self.trange[1], alpha=0.1, color="red")
-        # h, edg = np.histogram(self.data[:,2], bins=self.tbins)
-        # ax.vlines(x=self.trange[0], ymin=0, ymax=h.max(), linestyles="dotted",
-        #           lw=3, color="red")
-        # ax.vlines(x=self.trange[1], ymin=0, ymax=h.max(), linestyles="dotted",
-        #           lw=3, color="red")
 
     def plot_1(self, ax=None):
         if ax is None:
             fig = plt.figure(figsize=(8, 6))
             ax = fig.add_subplot()
-        h, edg = np.histogram(
+        self.spect_cts, edg = np.histogram(
             self.df["channel"], bins=self.ebins, range=[0, self.ebins]
         )
-        ax.plot(h, label=f"time range: {self.trange} us")
+        ax.plot(self.spect_cts, label=f"time range: {self.trange} us")
         ax.set_xlabel("Channels")
         ax.set_ylabel("Counts")
         ax.legend()
+
+    def plot_spect_e(
+        self,
+        plot_pulse=False,
+        ax=None,
+        pulse_width=100,
+        pulse_delay=20,
+        scale_pulse=2500,
+    ):
+        if ax is None:
+            fig = plt.figure(figsize=(8, 6))
+            ax = fig.add_subplot()
+        if self.erange is not None:
+            ax.hist(self.df["dt"], bins=self.dt_bins, edgecolor="black")
+            ax.set_title(f"Channel range: {self.erange}")
+            ax.set_xlabel("Time [us]")
+            ax.set_ylabel("Counts")
+
+            # include square pulse
+            if plot_pulse:
+                stp = np.ones(self.period)
+                stp[pulse_width:] = 0
+                stp_shift = np.roll(stp, pulse_delay)
+                t_shift = np.linspace(0, self.period, num=stp_shift.shape[0])
+                ax.plot(t_shift, stp_shift * scale_pulse, label="neutron pulse")
+                ax.legend(loc="upper right")
