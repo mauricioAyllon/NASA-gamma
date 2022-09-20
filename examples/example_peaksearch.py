0942752258ebf4b80ee73bb664c38a9eff5f131d
mauricioAyllon <58120060+mauricioAyllon@users.noreply.github.com>
2022-09-20 08:21:05 -0400

    Changed example file for easier read

--- examples/example_peaksearch.py
+++ examples/example_peaksearch.py
@@ -7,33 +7,24 @@ Example of peaksearch functionality
 """
 
 from nasagamma import spectrum as sp
-import numpy as np
 import pandas as pd
 from nasagamma import peaksearch as ps
 
 # dataset 1
-file = "data/SSR-mcnp.hdf"
-df = pd.read_hdf(file, key="data")
-
-# delete first (large) bin
-df = df.iloc[1:, :]
-
-cts_np = df.cts.to_numpy() * 1e8
-erg = np.array(df.index)
+file = "data/gui_test_data_cebr.csv"
+df = pd.read_csv(file)
 
 # instantiate a Spectrum object
-spect = sp.Spectrum(counts=cts_np, energies=erg)
+spect = sp.Spectrum(counts=df["counts"])
 
 # Required input parameters (in channels)
 fwhm_at_0 = 1
-ref_fwhm = 35
-ref_x = 1220
+ref_fwhm = 20
+ref_x = 420
 
 # instantiate a peaksearch object
-search = ps.PeakSearch(spect, ref_x, ref_fwhm, fwhm_at_0, min_snr=0.1)
-
+search = ps.PeakSearch(spect, ref_x, ref_fwhm, fwhm_at_0, min_snr=5)
+# plot kernel, peaks, and components
 search.plot_kernel()
-
 search.plot_peaks()
-
 search.plot_components()
