ce9925425f8450e372449b579c5c906abe3af11a
Ayllon Unzueta <mayllonu@ndc.nasa.gov>
2023-10-25 10:46:01 -0400

    Made very little progress on advanced fitting

--- nasagamma/advanced_fit.py
+++ nasagamma/advanced_fit.py
@@ -1,8 +1,15 @@
-'''
+"""
 Advanced fitting functions
-'''
+"""
 import matplotlib.pyplot as plt
 import numpy as np
 from nasagamma import file_reader
+from nasagamma import spectrum as sp
+from nasagamma import peaksearch as ps
+from nasagamma import peakfit as pf
 
-file_reader.read_csv_file("../examples/data/")
+
+spe1 = file_reader.read_csv_file("../examples/data/gui_test_data_cebr.csv")
+xrange = [1200, 1400]
+x = spe1.channels[xrange[0] : xrange[1]]
+y = spe1.counts[xrange[0] : xrange[1]]
