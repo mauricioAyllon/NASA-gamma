d5f1b67840c0c16389a88b903ba7f244b6f61ff5
Mauricio Ayllon <mauri.ayllon12@gmail.com>
2024-08-10 21:00:35 -0700

    Added an example file to read tlist files

---
+++ examples/example_read_tlist.py
@@ -0,0 +1,33 @@
+"""
+Example tlist
+"""
+
+from nasagamma import tlist
+from nasagamma import decay_exponential as decay
+import numpy as np
+import matplotlib.pyplot as plt
+import lmfit
+
+
+file = "data/gui_test_data_tlist.txt"
+png = tlist.Tlist(fname=file, period=1000)
+png.plot_time_hist()
+x, spe = png.hist_erg()
+png.plot_spect_erg_all(x=x, y=spe)
+
+
+# Die-away
+png.filter_tdata(trange=[210,1000])
+png.hist_time()
+png.plot_die_away()
+
+# Exponential fitting
+x_data = png.xd
+y_data = png.yd
+y_err = png.yerrd
+
+exp = decay.Decay_exp(x=x_data, y=y_data, yerr=y_err)
+exp.fit_single_decay()
+exp.plot(show_components=True)
+exp.fit_double_decay()
+exp.plot(show_components=True)
