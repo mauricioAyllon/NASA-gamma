607f6f5a845c7fadbccaf7855cea2258d62b28d0
Mauricio Ayllon Unzueta <mayllonu@lbl.gov>
2024-12-07 16:28:17 -0800

    First attempt a working piece of code for plotting MCA from the PIXIE16 (npy files)

--- examples/example_mca_pixie.py
+++ examples/example_mca_pixie.py
@@ -1,6 +1,7 @@
 """
 Read MCA data from PIXIE formatted as npy files.
 """
+
 from nasagamma import helper_api
 import json
 import matplotlib.pyplot as plt
@@ -12,15 +13,21 @@ runnr = 0
 mca = helper_api.read_mca(date="2024-05-15", runnr=runnr)
 
 plt.figure()
-for i,row in enumerate(mca):
+for i, row in enumerate(mca):
     plt.plot(row, label=f"Spectrum #{i}")
 plt.legend()
 plt.yscale("log")
 
-ch = 0
+ch = 9
 time = helper_api.read_mca_time(date, runnr, ch=ch, key="live")
+print(f"Live time = {time} s")
 
 file_path = helper_api.find_data_path(date, runnr)
 files = sorted(list(file_path.glob("MCA-data/*-stats-*")))
 d = helper_api.read_json(files[0])
-
+info = []
+for k in d.keys():
+    if type(d[k]) is list:
+        info.append((k, d[k][ch]))
+    else:
+        info.append((k, d[k]))
