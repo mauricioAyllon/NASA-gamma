ea5635f43d367f413b8ac5f0071872d616b358fc
Mauricio Ayllon Unzueta <mauri.ayllon12@gmail.com>
2026-03-03 17:20:41 -0800

    Added tested examples of peaksearch and its functionalities

---
+++ examples/peaksearch/example_peaksearch_cebr.py
@@ -0,0 +1,40 @@
+"""
+Example of peaksearch functionality for a CeBr3 detector
+"""
+from nasagamma import peaksearch as ps
+from nasagamma import file_reader
+
+
+# dataset
+file = "../data/gui_test_data_cebr.csv"
+# instantiate a Spectrum object
+spect = file_reader.read_csv_file(file)
+
+# Required input parameters (in channels)
+fwhm_at_0 = 1
+ref_fwhm = 20
+ref_x = 420
+
+# instantiate a peaksearch object
+search = ps.PeakSearch(spect, ref_x, ref_fwhm, fwhm_at_0, min_snr=5, method="km")
+# plot kernel, peaks, and components
+search.plot_kernel()
+search.plot_peaks()
+search.plot_components()
+
+# instantiate a peaksearch object with predetermined range
+search1 = ps.PeakSearch(
+    spect, ref_x, ref_fwhm, fwhm_at_0, min_snr=5, xrange=[1200, 1600], method="km"
+)
+search1.plot_peaks(snrs="off")
+
+# Print parameters/metadata
+print(search1.metadata())
+
+# Print peak positions in a given range
+search.peaks_in_range(x_min=0, x_max=300)
+
+# Export peak positions found and other data to a csv file
+#search.to_csv("tes_peaksearch.csv")
+
+
---
+++ examples/peaksearch/example_peaksearch_hpge.py
@@ -0,0 +1,39 @@
+"""
+Example of peaksearch functionality for a HPGe detector
+"""
+from nasagamma import peaksearch as ps
+from nasagamma import file_reader
+import time
+
+
+# dataset 
+file = "../data/gui_test_data_hpge_NH3.txt"
+# instantiate a Spectrum object
+spect = file_reader.read_txt(file)
+
+# Required input parameters (in channels)
+fwhm_at_0 = 1
+ref_fwhm = 5
+ref_x = 420
+
+# instantiate a peaksearch object with kernel method
+# Note: kernel method is usually more precise, but takes longer
+t0 = time.time()
+search = ps.PeakSearch(spect, ref_x, ref_fwhm, fwhm_at_0, min_snr=20, method="km")
+t1 = time.time() - t0
+print(f"Kernel method: {t1} s")
+
+search.plot_peaks()
+
+# instantiate a peaksearch object with fast method
+t2 = time.time()
+search1 = ps.PeakSearch(
+    spect, ref_x, ref_fwhm, fwhm_at_0, min_snr=20, method="fast"
+)
+t3 = time.time()-t2
+print(f"Fast method: {t3} s")
+
+search1.plot_peaks()
+
+
+
