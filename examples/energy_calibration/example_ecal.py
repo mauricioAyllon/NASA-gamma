850b074bf5900865b54075f439d1f7657575e153
Mauricio Ayllon Unzueta <mauri.ayllon12@gmail.com>
2026-04-12 23:09:24 -0700

    Added apply_calibration to example. Note: this example needs some revision

--- examples/energy_calibration/example_ecal.py
+++ examples/energy_calibration/example_ecal.py
@@ -18,17 +18,16 @@ ref_x = 1315
 ref_fwhm = 42
 search = ps.PeakSearch(spect, ref_x, ref_fwhm, fwhm_at_0, min_snr=4)
 
-xrange = [1250, 1600]
-bkg = "poly2"
+xrange = [680, 1600]
+bkg = "poly1"
 fit = pf.PeakFit(search, xrange=xrange, bkg=bkg)
 fit.plot()
 
 # energy calibration
 ch = spect.channels
 peak_info = fit.peak_info
-mean_values = [peak_info[0]["mean"], peak_info[1]["mean"]]
-mean_values.insert(0, 0)  # add the origin
-erg = [0, 1173.2, 1332.5]  # in keV
+mean_values = [peak_info[0]["mean"], peak_info[2]["mean"], peak_info[3]["mean"]]
+erg = [662, 1173.2, 1332.5]  # in keV
 
 cal = ecal.EnergyCalibration(mean_vals=mean_values, erg=erg, channels=ch,
                              n=1, e_units="keV")
@@ -36,7 +35,10 @@ cal = ecal.EnergyCalibration(mean_vals=mean_values, erg=erg, channels=ch,
 cal.plot()
 predicted_energies = cal.predicted
 
-spect_cal = sp.Spectrum(counts=spect.counts, energies=predicted_energies, e_units="keV")
-spect_cal.plot()
-plt.title("Calibrated spectrum")
+# spect_cal = sp.Spectrum(counts=spect.counts, energies=predicted_energies, e_units="keV")
+# spect_cal.plot()
+# plt.title("Calibrated spectrum")
 
+spect.apply_calibration(cal)
+spect.plot()
+plt.title("Calibrated spectrum")
